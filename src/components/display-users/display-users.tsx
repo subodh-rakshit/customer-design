918ea751a07c6c59ff9a21de3cba8b9932e45768
subodh-rakshit <r2609rakshit@gmail.com>
2023-03-08 16:18:09 +0530

    CSS added and implementation of the whole project

--- src/components/display-users/display-users.tsx
+++ src/components/display-users/display-users.tsx
@@ -7,6 +7,7 @@ type DisplayUserType = {
   name: string;
   role: string;
 };
+
 const DisplayUsers = () => {
   const [userType, setUserType] = useState<string>("ADMIN");
   const [dataFromCustomerApi, setDataFromCustomerApi] = useState<
@@ -37,48 +38,77 @@ const DisplayUsers = () => {
 
   return (
     <>
-      <div>User Types</div>
-      <ul>
-        <li className="flex flex-row">
+      <div className="mt-10 mx-10 mb-5 font-medium text-2xl">
+        User Types
+      </div>
+      <ul className="mx-10">
+        <li
+          className={`flex flex-row ${
+            userType === "ADMIN" && "bg-light_blue_background"
+          } rounded-lg`}
+        >
           <input
             type="radio"
             checked={userType === "ADMIN"}
+            className="ml-4"
             onChange={() => {
               setUserType("ADMIN");
               filterData(dataFromCustomerApiClone, "ADMIN");
             }}
           />
-          <p>Admin</p>
+          <p className="p-4">Admin</p>
         </li>
-        <li className="flex flex-row">
+        <li
+          className={`flex flex-row ${
+            userType === "MANAGER" && "bg-light_blue_background"
+          } rounded-lg`}
+        >
           <input
             type="radio"
             checked={userType === "MANAGER"}
+            className="ml-4"
             onChange={() => {
               setUserType("MANAGER");
               filterData(dataFromCustomerApiClone, "MANAGER");
             }}
           />
-          <p>Manager</p>
+          <p className="p-4">Manager</p>
         </li>
+        <br />
+        <br />
+        <hr className="text-horizontal_line" />
       </ul>
-      <div>Admin Users</div>
-      {dataFromCustomerApi.map((values, index) => {
-        return (
-          <>
-            <div className="flex flex-row">
-              <p>{values.name.substring(0,1).toUpperCase()}</p>
-              <div>
-                <p>{values.name}</p>
-                <p>
-                  {values.role.substring(0, 1).toUpperCase() +
-                    values.role.substring(1, values.role.length).toLowerCase()}
+      <div className="mt-10 mx-10 mb-5 font-medium text-2xl">
+        Admin Users
+      </div>
+      {dataFromCustomerApi.length === 0 ? (
+        <p className="flex items-center justify-center font-medium text-xl">
+          Loading...
+        </p>
+      ) : (
+        dataFromCustomerApi.map((values, index) => {
+          return (
+            <>
+              <div className="flex flex-row mx-10 my-5">
+                <p className="flex bg-light_blue_background items-center p-4 mr-4 rounded-lg text-single_character">
+                  {values.name.substring(0, 1).toUpperCase()}
                 </p>
+                <div className="flex flex-col justify-center">
+                  <p className="text-xl">{values.name}</p>
+                  <p className="text-user_type">
+                    {values.role.substring(0, 1).toUpperCase() +
+                      values.role
+                        .substring(1, values.role.length)
+                        .toLowerCase()}
+                  </p>
+                </div>
               </div>
-            </div>
-          </>
-        );
-      })}
+            </>
+          );
+        })
+      )}
+      <br />
+      {dataFromCustomerApi.length !== 0 && <hr className="mr-10 ml-10 text-horizontal_line" />}
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      single_character: "#0370CE",
+      light_blue_background: "#E8F2FB",
+      user_type: "#7f7e7c",
+      horizontal_line: "#e2e8f0"
+    },
     extend: {},
   },
   plugins: [],
-}
+};
